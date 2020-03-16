343fb6f8c4ebbcf821a1721a0027bdd17ef94f90
Anton Prakapovich <anton.prakapovich@gmail.com>
2020-03-17 02:04:37 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  if ( !Array.isArray(members) ) return false;
+  let arr = [];
+  for ( let i = 0; i < members.length; i++ ) {
+    if ( typeof members[i] === "string" ) {
+      members[i] = members[i].trim();
+      arr.push(members[i][0].toUpperCase());
+    }
+  }
+  return arr.sort().join("");  
 };
